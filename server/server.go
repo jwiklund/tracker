dcb31ffb69b91e70300182fdc07123241c66c20e
Jonas Wiklund <jonas.wiklund@atex.com>
2013-10-02 12:17:37 +0200

    added atom wrapper and fixed running in openshift

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
-package main
+package server
 
 import (
+	"fmt"
 	"github.com/emicklei/go-restful"
 	"github.com/jwiklund/tracker/rss"
 	"github.com/jwiklund/tracker/source"
@@ -10,18 +11,25 @@ import (
 
 func Tracker() *restful.WebService {
 	ws := new(restful.WebService)
-	ws.Path("/").Produces(restful.MIME_XML)
-	ws.Route(ws.GET("/questionable").To(questionable))
+	ws.Path("/")
+	ws.Route(ws.GET("/questionable").To(questionable)).Produces(restful.MIME_XML)
 	return ws
 }
 
 func questionable(request *restful.Request, response *restful.Response) {
 	q := source.NewQuestionable()
+	response.Write([]byte("<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?>"))
+	response.Write([]byte("<rss xmlns:atom=\"http://www.w3.org/2005/Atom\" version=\"2.0\">"))
 	rss.Write(q, response)
+	response.Write([]byte("</rss>"))
 }
 
-func main() {
+func Run(ip, port string) {
+	if port == "" {
+		port = "8080"
+	}
+	list := fmt.Sprintf("%s:%s", ip, port)
 	restful.Add(Tracker())
-	log.Printf("start listening on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("start listening on " + list)
+	log.Fatal(http.ListenAndServe(list, nil))
 }
