81e49389fb9a4886e30b6bc935f0a0316607aa16
Jonas Wiklund <jonas.wiklund@atex.com>
2013-10-11 15:57:28 +0200

    Added filter to horrible

--- server/server.go
+++ server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jwiklund/tracker/source"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func Tracker(hs *source.HorribleSource) *restful.WebService {
@@ -16,8 +17,17 @@ func Tracker(hs *source.HorribleSource) *restful.WebService {
 	ws.Route(ws.GET("/sinfest").To(handler(source.NewSinfest())))
 	ws.Route(ws.GET("/xkcd").To(handler(source.NewXKCD())))
 	ws.Route(ws.GET("/horrible").To(handler(source.NewHorrible(hs, ""))))
+	ws.Route(ws.GET("/horrible/{serie}").Param(ws.PathParameter("serie", "serie")).To(horribleHandler(hs)))
 	ws.Route(ws.GET("/dailyshow").To(handler(source.NewDailyShow())))
 	return ws
+}
+
+func horribleHandler(hs *source.HorribleSource) func(*restful.Request, *restful.Response) {
+	f := func(request *restful.Request, response *restful.Response) {
+		ser := strings.Replace(request.PathParameter("serie"), "+", " ", -1)
+		handler(source.NewHorrible(hs, ser))(request, response)
+	}
+	return f
 }
 
 func handler(s source.Source) func(*restful.Request, *restful.Response) {
