8e8a3402f3651203a30f97a987c1b0debc5bc7c0
Jonas Wiklund <jwiklund@gmail.com>
2015-08-09 22:13:21 +0200

    Remove unused code

--- src/main/java/so/born/tracker/Html.java
+++ src/main/java/so/born/tracker/Html.java
@@ -9,10 +9,7 @@ import org.jsoup.nodes.Document;
 import com.sun.jersey.api.client.WebResource;
 
 public class Html {
-    private static final String USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537";
-
     public static Document fetch(WebResource resource, String url) throws IOException {
-        resource.header("User-Agent", USER_AGENT);
         try (InputStream data = resource.get(InputStream.class)) {
             return Jsoup.parse(data, "utf8", url);
         }
